74c9ef3da9142ab3c89608531d639740c94f6f77
Daniel Gomes Falcao <danielgomesfalcao@gmail.com>
2023-01-16 08:43:44 -0300

    Github auth done

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,26 +1,28 @@
+import { signIn, signOut, useSession } from 'next-auth/react';
 import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
 export function SignInButton(){
 
-    const session = false;
+    const { data: session } = useSession()
+
 
     return session ? (
         <button
             type='button'
             className={styles.signInButton}
-            onClick={() => {}}
+            onClick={() => signOut()}
         >
-            <img src='https://sujeitoprogramador.com/steve.png' alt='User photo' />
-            Hello Daniel
+            <img src={session.user!.image!} alt='User photo' />
+            Hello {session.user!.name}
             <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
         <button
             type='button'
             className={styles.signInButton}
-            onClick={() => {}}
+            onClick={() => signIn('github')}
         >
             <FaGithub color="#FFB800" />
             Login with Github
